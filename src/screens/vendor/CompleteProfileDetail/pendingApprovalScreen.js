081be70a14001f35a88c38f8e53239887816d2b1
Alok Singh <thealoksinghh@gmail.com>
2025-03-27 01:28:22 +0530

    some issue resolved to build apk but still there are issues

--- src/screens/vendor/CompleteProfileDetail/pendingApprovalScreen.js
+++ src/screens/vendor/CompleteProfileDetail/pendingApprovalScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
+import { logoutUser } from "../../../redux/store/userSlice";
+import {  useDispatch } from "react-redux";
 const PendingApprovalScreen = () => {  
   const navigation = useNavigation();  
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.container}>
@@ -12,8 +14,8 @@ const PendingApprovalScreen = () => {
       <Text style={styles.subtitle}>Kindly wait for approval from the admin.{"\n"}This usually takes 2-3 business days.</Text>
      
 
-      <TouchableOpacity style={styles.button} onPress={() => Alert.alert("Kindly wait for approval")}>
-        <Text style={styles.buttonText}>Logout</Text>
+      <TouchableOpacity style={styles.button} onPress={() => dispatch(logoutUser())}>
+      <Text style={styles.buttonText}>Logout</Text>
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.goBack()}>
